2376cbe6aa15d866be2d9603b725049c35435888
David <dpuerta@usal.es>
2021-12-14 20:29:48 +0100

    bookLooper function created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log("hello world")
 
+let myLibrary = []
+
 function Book(title,author,pages,read){
     this.title = title
     this.author = author
@@ -8,6 +10,26 @@ function Book(title,author,pages,read){
     this.info = () => console.log(this.title, "by", this.author, ",", this.pages, "pages, and its", this.read)
 }
 
+function addBookToLibrary(){
+    const titleInput = window.prompt("Introduce title")
+    const authorInput = window.prompt("Introduce author")
+    const pagesInput = window.prompt("Introduce pages")
+    const readInput = window.prompt("Introduce read or not")
+    const bookObject = new Book(titleInput, authorInput, pagesInput, readInput)
+    myLibrary = myLibrary.concat(bookObject)
+}
+
 const theHobbit = new Book("The Hobbit", "Tolkien", "295", "not read yet")
-console.log(theHobbit.title)
-theHobbit.info()
+myLibrary = myLibrary.concat(theHobbit)
+
+const bookContainer = document.querySelector(".bookContainer")
+
+const bookLooper = () => {
+    myLibrary.map(book => {
+        const newBookDiv = document.createElement("div")
+        newBookDiv.textContent = book.title
+        bookContainer.append(newBookDiv)
+    })
+}
+
+bookLooper()
