5bd06d6378be6c269206a1ac211b1e9cad311984
David <dpuerta@usal.es>
2021-12-14 21:52:45 +0100

    Code restructured Added bootstrap

--- index.js
+++ index.js
@@ -2,46 +2,58 @@ console.log("hello world")
 
 let myLibrary = []
 
-const refreshBooks = () => {
-    console.log(myLibrary)
+// we need to first destroy books and then create new ones or we will get duplicate divs
+const destroyBookContainer = () => {
     const bookContainerOld = document.querySelector(".bookContainer")
     bookContainerOld.parentElement.removeChild(bookContainerOld)
     const wholeWeb = document.querySelector(".webContainer")
     const bookContainer = document.createElement("div")
     bookContainer.className = "bookContainer"
     wholeWeb.append(bookContainer)
+}
 
-    myLibrary.map(book => {
-        const newBookDiv = document.createElement("div")
-        newBookDiv.textContent = book.title
-        newBookDiv.id = myLibrary.indexOf(book)
-        const newBookDiv2 = document.createElement("div")
-        newBookDiv2.textContent = "by " + book.author
-        const newBookDiv3 = document.createElement("div")
-        newBookDiv3.textContent = book.pages + " pages"
-        const newBookDiv4 = document.createElement("div")
-        newBookDiv4.textContent = book.read
-        const removeButton = document.createElement("button")
-        removeButton.setAttribute("data", myLibrary.indexOf(book))
-        removeButton.textContent="Remove"
-        newBookDiv.append(removeButton)
-        removeButton.addEventListener("click", ()=>{
-            myLibrary.splice(Number(removeButton.getAttribute("data")), 1)
-            newBookDiv.parentElement.removeChild(newBookDiv)
-        }) 
-        const changeStatus = document.createElement("button")
-        changeStatus.setAttribute("data", myLibrary.indexOf(book))
-        changeStatus.textContent="Change status"
-        changeStatus.addEventListener("click", ()=>{
-            myLibrary[Number(changeStatus.getAttribute("data"))].change()
-            refreshBooks()
-        })
-        newBookDiv.append(changeStatus)
-        newBookDiv.append(newBookDiv2)
-        newBookDiv.append(newBookDiv3)
-        newBookDiv.append(newBookDiv4)
-        bookContainer.append(newBookDiv)
+const createNewDiv = (textContent) => {
+    const newDiv = document.createElement("div")
+    newDiv.textContent = textContent
+    return newDiv
+}
+
+const createBookCard = (book, bookContainer) => {
+    const newBookDiv = document.createElement("div")
+    newBookDiv.textContent = book.title
+    newBookDiv.id = myLibrary.indexOf(book)
+    const newBookDiv2 = createNewDiv(`by ${book.author}`)
+    const newBookDiv3 = createNewDiv(`${book.pages} pages`)
+    const newBookDiv4 = createNewDiv(book.read)
+    const removeButton = document.createElement("button")
+    removeButton.setAttribute("data", myLibrary.indexOf(book))
+    removeButton.textContent="Remove"
+    const changeStatus = document.createElement("button")
+    changeStatus.setAttribute("data", myLibrary.indexOf(book))
+    changeStatus.textContent="Change status"
+    
+    newBookDiv.append(removeButton)
+    newBookDiv.append(changeStatus)
+    newBookDiv.append(newBookDiv2)
+    newBookDiv.append(newBookDiv3)
+    newBookDiv.append(newBookDiv4)
+    
+    removeButton.addEventListener("click", ()=>{
+        myLibrary.splice(Number(removeButton.getAttribute("data")), 1)
+        newBookDiv.parentElement.removeChild(newBookDiv)
+    }) 
+    changeStatus.addEventListener("click", ()=>{
+        myLibrary[Number(changeStatus.getAttribute("data"))].change()
+        refreshBooks()
     })
+    
+    bookContainer.append(newBookDiv)
+}
+
+const refreshBooks = () => {
+    destroyBookContainer()
+    const bookContainer = document.querySelector(".bookContainer")
+    myLibrary.map(book => createBookCard(book,bookContainer))
 }
 
 function Book(title,author,pages,read){
@@ -70,10 +82,7 @@ function addBookToLibrary(){
 const theHobbit = new Book("The Hobbit", "Tolkien", "295", "not read")
 myLibrary = myLibrary.concat(theHobbit)
 
-const bookContainer = document.querySelector(".bookContainer")
 const createButton = document.querySelector(".create")
-
-
 
 refreshBooks()
 createButton.addEventListener("click", ()=>addBookToLibrary())
